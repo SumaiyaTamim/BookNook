bc8babbbccef0080b528807da3d3cc62e6603395
Tamim <tamimsumaiya2002@gmail.com>
2025-04-25 22:39:47 +0800

    database and backend initial setup done

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+// const express = require('express')
+// const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config();
+
+const PORT = process.env.PORT || 4000
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+
+app.get('/', (req, res) => {
+  res.send('Hello YOU!')
+})
+
+app.listen(PORT, () => { 
+  console.log(`Server is  listening on port ${PORT}`)
+})
